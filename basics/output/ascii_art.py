2a68b0666885a8be70813fa36b6f303e126c8ac9
Lawrence <lawrence.dawson@hotmail.com>
2021-02-05 10:40:12 +0000

    Added code to create an Ascii robot

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,22 @@
+# display beep in code
+print("=========")
+print("|\t\t|")
+print("|\t00\t|")
+print("|\t\t|")
+print("|\t##\t|")
+print("=========")
+print("\t||\t")
+print("=========")
+print("|\t\t|")
+print("|\t\t|")
+print("|\tBeep|")
+print("|\t\t|")
+print("|\t\t|")
+print("|\t\t|")
+print("=========")
+
+
+
+
+
+
