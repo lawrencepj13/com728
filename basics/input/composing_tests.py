c2fe134b8ec6f4f6a02ce14098eb2cb04d5b21f3
Lawrence <lawrence.dawson@hotmail.com>
2021-02-10 12:31:58 +0000

    Created a script to demonstrate some learning from Think Python book - composing and definitions

---
+++ basics/input/composing_tests.py
@@ -0,0 +1,25 @@
+def print_lyrics():
+    print("I am a lumberjack")
+    print("I've been working all day")
+
+
+print_lyrics()
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+repeat_lyrics()
+
+
+
+
+
+
+
+
+
+
+
