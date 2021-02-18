4d0807f0cf850d63d97f10c8606773179b7d2d2b
Lawrence <lawrence.dawson@hotmail.com>
2021-02-18 11:55:01 +0000

    Added code to demonstrate the use of one loop nested is another.

---
+++ basics/repetitions/for_loop/membership_operators.py
@@ -0,0 +1,11 @@
+print("What phrase do you see?")
+print()
+phrase = input()
+print()
+print("Reversing...")
+print("The phrase is...", end="")
+reversed = ""
+for letter in phrase:
+    revered = letter + reversed
+print(reversed)
+
