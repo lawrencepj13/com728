316bcaf512bb5ff93d0608780241a4712e6fdb9a
Lawrence <lawrence.dawson@hotmail.com>
2021-02-19 10:36:04 +0000

    Added code to demonstrate the use of a user-defined function

---
+++ basics/functions/simple.py
@@ -0,0 +1,8 @@
+# define function for listening to sounds
+def listen_to_sound():
+    print("What sound did I hear?")
+    sound = input()
+    print(f"That was a loud {sound}")
+#call the function
+listen_to_sound()
+
