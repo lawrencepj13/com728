660d2ce4b9aa5dc4b7cd5ab0ccb29f42db78e44b
Lawrence <lawrence.dawson@hotmail.com>
2021-02-26 11:32:03 +0000

    Added code to read a text file line by line.

---
+++ data/files/txt/read_lines.py
@@ -0,0 +1,9 @@
+def search(path):
+    with open(f"{path}") as file:
+        print("Searching...")
+        for line in file:
+            print(f"Looked in {line}")
+        print("...Done!")
+def run():
+    search("library.txt")
+run()
