c9ad00914207eaf92c1009af4521405bb6412177
Lawrence <lawrence.dawson@hotmail.com>
2021-03-05 11:25:10 +0000

    Added code to demonstrate how to nest tuples and lists

---
+++ data/tuples/nested_tuple.py
@@ -0,0 +1,19 @@
+def steps():
+    likelihood = [("step 1", 50), ("step 2", 38) ,("step 3", 27) ,("step 4", 99) ,("step 5", 4)]
+    return likelihood
+
+
+def run():
+    choices = steps()
+    good_steps = []
+    bad_steps = []
+
+    for step in choices:
+        if (step[1]<=50):
+            bad_steps.append(step)
+        else:
+            good_steps.append(step)
+    print(f"Good steps: {len(good_steps)}, bad steps: {len(bad_steps)}")
+
+run()
+
