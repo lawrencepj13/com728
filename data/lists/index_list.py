a00de45e4ccefd47007db9eff79ab5f04bf195bc
Lawrence <lawrence.dawson@hotmail.com>
2021-03-05 10:35:27 +0000

    Added code to demonstrate how to index a list.

--- data/lists/index_list.py
+++ data/lists/index_list.py
@@ -5,5 +5,12 @@ def movements():
 
 def run():
     print("Moving...")
-    direction = movements()
-    print("")
+    path = movements()
+    print(f"{path[0]} for {path[1]} steps")
+    print(f"{path[2]} for {path[3]} steps")
+    print(f"{path[4]} for {path[5]} steps")
+    print(f"{path[6]} for {path[7]} steps")
+
+
+run()
+
