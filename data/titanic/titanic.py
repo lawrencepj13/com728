cc93d48a1baeec48c67ef00cda0a72b135109ab2
Lawrence <lawrence.dawson@hotmail.com>
2021-03-08 10:42:57 +0000

    Added code to display a menu of options

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -14,12 +14,29 @@ def load_data(file_path):
         return records
 
 
+def display_menu():
+    print("""
+      Please select one of the following options:
+  [1] Display the names of all passengers
+  [2] Display the number of passengers that survived
+  [3] Display the number of passengers per gender
+  [4] Display the number of passengers per age group
+  """)
+    response = int(input())
+    return response
+
+
 
 def run():
     file = load_data("titanic.csv")
     num_records = records
     records.append(load_data("titanic.csv"))
     print(f"Succesfully loaded {num_records} records.")
+    menu = display_menu()
+    selected_response = menu
+    print(f"You have selection option: {selected_response}")
+
+
 
 
 
