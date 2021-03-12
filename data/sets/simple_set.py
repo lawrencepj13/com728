9efaf12c67dd64eab036e5771aace4903cc99bef
Lawrence <lawrence.dawson@hotmail.com>
2021-03-12 09:59:51 +0000

    Added code to demonstrate a simple set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,11 @@
+def obeserved():
+    observations = set()
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+
+def run():
+    print(obeserved())
+
+
+run()
