03dea032988fdaf321b325c5ce079f7a4462989b
Lawrence <lawrence.dawson@hotmail.com>
2021-03-26 11:27:43 +0000

    "Added code to retrieve data using an inner join."

--- data/persistence_2/database.py
+++ data/persistence_2/database.py
@@ -12,3 +12,15 @@ def display_products_with_stock_levels():
     db.close()
 
 
+def display_product_supplier():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = "SELECT name " \
+          "FROM product " \
+          "INNER JOIN supplier ON product.supplier_id = supplier.supplier_name"
+    cursor.execute(sql)
+    table = cursor.fetchall()
+    print(table)
+    db.close()
+
+
--- data/persistence_2/main.py
+++ data/persistence_2/main.py
@@ -3,7 +3,8 @@ import database
 
 def menu():
     print("Please enter one of the following options:")
-    print("[1] Display stock levels")
+    print("""[1] Display stock levels
+    [2]Display suppliers""")
     print()
     print("Your selection:")
     response = input()
@@ -19,6 +20,11 @@ def run():
             print("There are 3 products in the catalogue")
             print("The stock levels for each product is as follows:")
             database.display_products_with_stock_levels()
+        elif selection == "2":
+            print(f"Your selection is {selection}")
+            print()
+            print("The suppliers for each product are as follows:")
+            database.display_product_supplier()
         break
 
 
